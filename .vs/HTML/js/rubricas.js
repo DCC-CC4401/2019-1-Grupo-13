e8f35975b3806a32036a353f38f19b9d28d48eee
nikom1912 <nikom_1912@hotmail.com>
2019-04-05 13:52:21 -0300

    init

---
+++ .vs/HTML/js/rubricas.js
@@ -0,0 +1,8 @@
+function rubricas() {
+    if (window.File && window.FileReader && window.FileList && window.Blob) {
+        
+         } else {
+            alert('The File APIs are not fully supported in this browser.');
+          }
+    }
+
